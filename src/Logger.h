6d1057da411ad7197d7a432f7992b0ed0704fe73
falco <falco@DESKTOP-9BUR4S7>
2022-11-24 16:23:14 +0300

    Add format overload for logger

--- src/Logger.h
+++ src/Logger.h
@@ -1,20 +1,35 @@
 #pragma once
-#include "Template/atSingleton.h"
+#include <cstdarg>
 #include <string>
 #include <fstream>
 #include <vector>
+#include "format"
+
+#include "Template/atSingleton.h"
 
 class Logger : public atSingleton<Logger>
 {
 	std::string logFile = "rageAm.txt";
 	std::vector<std::string> entries;
 public:
-	void Log(const std::string& msg, bool truncate = false)
+	void Init()
 	{
-		auto fs = Open(truncate);
+		Open(true).close();
+	}
+
+	void Log(const std::string& msg)
+	{
+		auto fs = Open(false);
 		fs << msg << std::endl;
 		entries.push_back(msg);
 		fs.close();
+	}
+
+	template<typename... Args>
+	void Log(std::_Fmt_string<Args...> fmt, Args&&... args)
+	{
+		// https://stackoverflow.com/questions/72795189/how-can-i-wrap-stdformat-with-my-own-template-function
+		Log(std::format(fmt, std::forward<Args>(args)...));
 	}
 
 	std::vector<std::string> GetEntries()
