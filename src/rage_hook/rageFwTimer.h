b992b30cf259e535f32fdaebdeeed495ed20801a
falco <falco@DESKTOP-9BUR4S7>
2022-12-07 03:27:35 +0300

    Fix fwTimer pattern

--- src/rage_hook/rageFwTimer.h
+++ src/rage_hook/rageFwTimer.h
@@ -13,15 +13,17 @@ namespace rh
 	public:
 		fwTimer()
 		{
-			gm::gmAddress addr = g_Scanner.ScanPattern("GameBacktraceConfig::WriteCrashContextToFile",
-				"48 83 EC 48 48 83 64 24 30 00 83 64 24 28 00 45");
+			gm::gmAddress addr = g_Scanner.ScanPattern("fwTimer::InitLevel",
+				"33 C0 C7 05 ?? ?? ?? ?? ?? ?? ?? ?? C7 05 ?? ?? ?? ?? ?? ?? ?? ?? C7");
 
-			addr = addr.GetAt(0x179 + 0x2).GetRef();
+			addr = addr.GetAt(0x30 + 0x2).GetRef();
 
-			gPtr_bGamePause = addr.CastAt<bool*>(-0x1);
-			gPtr_bUserPause = addr.CastAt<bool*>(0x0);
-			gPtr_bScriptPause = addr.CastAt<bool*>(0x1);
-			gPtr_bSystemPause = addr.CastAt<bool*>(0x2);
+			g_Log.LogD("fwTimer: {:X}", addr.GetAddress());
+
+			gPtr_bGamePause = addr.CastAt<bool*>(0x0);
+			gPtr_bUserPause = addr.CastAt<bool*>(0x1);
+			gPtr_bScriptPause = addr.CastAt<bool*>(0x2);
+			gPtr_bSystemPause = addr.CastAt<bool*>(0x3);
 		}
 	};
 
