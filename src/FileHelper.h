07b90eba5854227e446ab3f3e4231fa06789e935
ranstar74 <falconr1125@gmail.com>
2023-01-04 08:18:28 +0300

    Add data folder in default folders

--- src/FileHelper.h
+++ src/FileHelper.h
@@ -1,12 +1,10 @@
 #pragma once
 #include "Windows.h"
 
-/**
- * \brief Creates 'rageAm' directory in GTA root folder and sub folders.
- */
-inline void EnsureDataFoldersExist()
+inline void CreateDefaultFolders()
 {
 	CreateDirectoryW(L"rageAm", nullptr);
 	CreateDirectoryW(L"rageAm/Textures", nullptr);
 	CreateDirectoryW(L"rageAm/Shaders", nullptr);
+	CreateDirectoryW(L"rageAm/Data", nullptr);
 }
