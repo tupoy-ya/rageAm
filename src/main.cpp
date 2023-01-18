0e3f509cd7044987f53bb60aa845eb29bca49b18
ranstar74 <falconr1125@gmail.com>
2023-01-18 13:20:24 +0300

    Resource reading test

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,7 @@ auto startTime = std::chrono::high_resolution_clock::now();
 #include "CrashHandler.h"
 #include "GameVersion.h"
 #include "gmScanner.h"
+#include "gmhook.h"
 /* COMMON */
 
 /* HOOK INCLUDES */
@@ -62,6 +63,94 @@ auto startTime = std::chrono::high_resolution_clock::now();
 #include "imgui_rage/sys_apps/ImGuiApp_Toolbar.h"
 #include "imgui_rage/sys_apps/ImGuiApp_Overlay.h"
 
+struct ResourceHeader
+{
+	u32 Magic;
+	u32 Version;
+	rage::datResourceInfo Info;
+};
+
+struct ResourceFile : ResourceHeader
+{
+	u8 Data;
+};
+static_assert(offsetof(ResourceFile, Data) == 0x10);
+
+#include "file/fiCollection.h"
+#include "zlib.h"
+void ReadResource()
+{
+	rage::fiPackfile* pack = rage::fiCollection::GetCollection(1);
+	rage::fiPackEntry* entry = pack->GetEntry("ranstar.ytd");
+
+	rage::datResourceInfo info = entry->GetResourceInfo();
+	rage::datResourceMap map{};
+	info.GenerateMap(map);
+
+	// 'Open' file
+	u32 offset = entry->GetOffset();
+	fiHandle_t file = pack->GetHandle();
+	rage::fiDevice* pDevice = pack->GetParent();
+
+	//u32 size = entry->GetOnDiskSize();
+	//u32 size2 = pDevice->vftable->Size(pDevice, file);
+	u32 size = 99'465; // TODO: How to get actual size?
+
+	char* buffer = new char[size];
+	pDevice->vftable->ReadOverlapped(pDevice, file, offset, buffer, size);
+
+	ResourceFile* rscFile = (ResourceFile*)buffer;
+	if (rscFile->Magic == 0x37435352 && rscFile->Version == 13) // RSC7, #TD ver. 13
+	{
+		// Init zlib state
+		z_stream st{};
+		if (inflateInit2(&st, -15) != Z_OK)
+			return;
+		st.avail_in = size;
+		st.next_in = &rscFile->Data;
+
+		// Allocate map & read chunks from resource file
+		for (u8 i = 0; i < map.VirtualChunkCount + map.PhysicalChunkCount; i++)
+		{
+			rage::datResourceChunk& chunk = map.Chunks[i];
+			Bytef* chunkMem = new Bytef[chunk.Size];
+
+			// Decompress chunk
+			st.avail_out = chunk.Size;
+			st.next_out = chunkMem;
+			if (inflate(&st, Z_FULL_FLUSH) < 0)
+				return;
+
+			chunk.DestAddr = (u64)chunkMem;
+		}
+		inflateEnd(&st);
+
+		// Main page is first virtual chunk
+		map.MainPage = (rage::pgBase*)map.Chunks[0].DestAddr;
+
+		// Place resource
+		rage::datResource rsc(map);
+		rage::pgDictionary<rage::grcTexture>* txd =
+			new(rsc) rage::pgDictionary<rage::grcTexture>(rsc);
+
+		AM_TRACE("ranstar.ytd:");
+		for (u16 i = 0; i < txd->GetCount(); i++)
+		{
+			const rage::pgKeyPair<rage::grcTexture*>& pair = txd->GetSlot(i);
+
+			AM_TRACEF("{} - [{:X}]: {}", i, pair.Key, pair.Value->GetName());
+		}
+	}
+
+	// Free map
+	for (u8 i = 0; i < map.VirtualChunkCount; i++)
+	{
+		rage::datResourceChunk& chunk = map.Chunks[i];
+		operator delete[]((void*)chunk.DestAddr, chunk.Size);
+	}
+	delete[] buffer;
+}
+
 AM_EXPORT void Shutdown()
 {
 	g_Log.LogT("main::Shutdown()");
@@ -75,6 +164,9 @@ AM_EXPORT void Init()
 {
 	g_Log.LogT("main::Init()");
 
+	// rage::hooks::RegisterResource();
+	rh::Rendering::RegisterRender();
+
 	g_ImGui.Init();
 	g_ImGuiAppMgr.Init();
 	g_ImGuiAppMgr.RegisterApp<sapp::ImGuiApp_Toolbar>();
@@ -82,6 +174,8 @@ AM_EXPORT void Init()
 
 	auto currentTime = std::chrono::high_resolution_clock::now();
 	g_Log.LogT("Core systems initialization took {}ms", std::chrono::duration<double, std::milli>(currentTime - startTime).count());
+
+	// CrashHandler::ExecuteSafe(ReadResource);
 }
 
 void OnAttach()
