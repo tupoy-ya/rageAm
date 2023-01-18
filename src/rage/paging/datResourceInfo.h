1aac3637a4024179d30342b0ba25144c91541789
ranstar74 <falconr1125@gmail.com>
2023-01-18 14:36:59 +0300

    Add doc for datResourceInfo

--- src/rage/paging/datResourceInfo.h
+++ src/rage/paging/datResourceInfo.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "datResourceMap.h"
-//#include "gmFunc.h"
+
+#ifndef RAGE_STANDALONE
+#include "gmFunc.h"
+#endif
 
 namespace rage
 {
+	/**
+	 * \brief Resource info is resource header that contains encoded virtual and physical chunks.
+	 */
 	struct datResourceInfo
 	{
 		u32 VirtualData;
@@ -11,32 +17,42 @@ namespace rage
 
 		static u8 GetSizeShift(u32 data);
 		static u8 GetNumChunks(u32 data);
+
+		/**
+		 * \brief Generates chunk definitions from encoded data.
+		 * \param data Encoded data. (Virtual or Physical).
+		 * \param baseSize Default page size, see dat.h;
+		 * \param map Destination map.
+		 * \param startChunk Number of chunks to skip in map.
+		 * \param baseAddr Chunk address base, see dat.h;
+		 * \return
+		 */
 		static u8 GenerateChunks(u32 data, u64 baseSize, datResourceMap& map, u8 startChunk, u64 baseAddr);
 
 		u8 GetNumVirtualChunks() const;
 		u8 GetNumPhysicalChunks() const;
 
+		/**
+		 * \brief Decodes virtual and physical data into resource offset map.
+		 * \param map Destination map.
+		 */
 		void GenerateMap(datResourceMap& map) const;
 	};
 
-//	namespace hooks
-//	{
-//		inline gm::gmFunc<void, const datResourceInfo*, datResourceMap*> gImpl_datResourceInfo_GenerateMap;
-//
-//		//static inline gm::gmFuncHook gSwap_datResourceInfo_GenerateMap(
-//		//	"rage::datResourceInfo::GenerateMap",
-//		//	"48 89 5C 24 08 57 48 83 EC 30 44 8B 09",
-//		//	&datResourceInfo::GenerateMap,
-//		//	&gImpl_datResourceInfo_GenerateMap);
-//
-//		static void RegisterResource()
-//		{
-//#ifdef RAGE_HOOK_SWAP_DATRESOURCE
-//
-//			gImpl_datResourceInfo_GenerateMap = gm::Hook("rage::datResourceInfo::GenerateMap",
-//				"48 89 5C 24 08 57 48 83 EC 30 44 8B 09",
-//				&datResourceInfo::GenerateMap);
-//#endif
-//	}
-//	}
+#ifndef RAGE_STANDALONE
+	namespace hooks
+	{
+		inline gm::gmFunc<void, const datResourceInfo*, datResourceMap*> gImpl_datResourceInfo_GenerateMap;
+
+		static void RegisterResourceInfo()
+		{
+#ifdef RAGE_HOOK_SWAP_DATRESOURCE
+			gImpl_datResourceInfo_GenerateMap = gm::Hook(
+				"rage::datResourceInfo::GenerateMap",
+				"48 89 5C 24 08 57 48 83 EC 30 44 8B 09",
+				&datResourceInfo::GenerateMap);
+#endif
+		}
+	}
+#endif
 }
